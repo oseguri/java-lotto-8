17def150c80deea4f51e42b07762c677c0b6c522
oseguri <canaein@gmail.com>
2025-11-02 21:28:56 +0900

    test(inputvalidatortest): 당첨 번호 검증 테스트 추가

--- src/test/java/lotto/InputValidatorTest.java
+++ src/test/java/lotto/InputValidatorTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class InputValidatorTest {
 
     @ParameterizedTest
-    @CsvSource(value = {" :false","0:false","999:false","1024:false","3000:true","10000:true"}, delimiter =':')
+    @CsvSource(value = {":false"," :false","0:false","999:false","1024:false","3000:true","10000:true"}, delimiter =':')
     void priceValidationTest(String price, String set) {
         if(set.equals("false")) {
             assertThatThrownBy(() -> InputValidator.validatePrice(price))
@@ -21,10 +21,17 @@ public class InputValidatorTest {
 
     }
 
-//    @ParameterizedTest
-//    void winNumberValidationTest() {
-//
-//    }
+    @ParameterizedTest
+    @CsvSource(value = {":false"," :false", "1,2,3,4,2,3:false", "1,2,3:false", "a,b,c,d,e,f:false", "1|2|3|4|5|6:false","1234:false","1,2,3,4,5,6:true","1,15,17,19,40,45:true"}, delimiter = ':')
+    void winNumberValidationTest(String input, String set) {
+        if(set.equals("false")) {
+            assertThatThrownBy(() -> InputValidator.isValidWinNum(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }else if(set.equals("true")) {
+            assertThatCode(() -> InputValidator.isValidWinNum(input))
+                    .doesNotThrowAnyException();
+        }
+    }
 //
 //    @ParameterizedTest
 //    void bonusNumberValidationTest() {
