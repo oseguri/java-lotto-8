7b0264c262b46fb8f3f67751349e997d8faae799
oseguri <canaein@gmail.com>
2025-11-02 23:11:53 +0900

    feat(lottocalculator): 로또 결과 계산 클래스 추가

---
+++ src/main/java/lotto/LottoCalculator.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class LottoCalculator {
+    private final static Map<Integer, Integer> winnerPrice = Map.of(
+            6,2000000000
+            ,5,1500000
+            ,4,500000
+            ,3,5000);
+
+    public static LottoResult getResult(List<Lotto> lottoList, List<Integer> winNums, int bonusNum) {
+        List<Integer> prices = new ArrayList<>();
+        lottoList.forEach((lotto) -> prices.add(calculate(lotto,winNums,bonusNum)));
+
+        return new LottoResult(prices);
+    }
+
+    private static int calculate(Lotto lotto, List<Integer> winNums, int bonusNum) {
+        List<Integer> lottoNums = lotto.getNumbers();
+
+        AtomicInteger correct = new AtomicInteger();
+        winNums.forEach(num -> {
+            if(lottoNums.contains(num)) correct.getAndIncrement();
+        });
+
+        int correctNum = correct.get();
+        if(winnerPrice.containsKey(correctNum)) return 0;
+
+        int price = winnerPrice.get(correctNum);
+        if(correctNum == 5 && lottoNums.contains(bonusNum)) price *= 200;
+        return price;
+    }
+}
